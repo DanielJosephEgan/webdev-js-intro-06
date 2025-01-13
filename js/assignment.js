d691c6157b62a8cd9f202a6afac32477431b704a
Daniel Joseph Merton Egan <CATHOLICTIMELINE@gmail.com>
2025-01-12 23:32:06 -0500

    First Commit Jan 12, 2025

--- js/assignment.js
+++ js/assignment.js
@@ -1,37 +1,45 @@
 "use strict";
 
 const submissionBtn = document.getElementById("submission-btn");
+
 const evenOrOddElement = document.getElementById("even-or-odd");
 const sumTheNumbersElement = document.getElementById("sum-the-numbers");
 const createNumberArrayElement = document.getElementById("create-number-array");
 
 function evenOrOdd() {
     const num = 3;
-    // Write the logic to decide if the variable "num" is even or odd
-    // and set the element's value the string "Even" or "Odd" accordingly
 
-}
+    if (num % 2 === 0) {
+    document.getElementById("even-or-odd").innerHTML = `${"Even"}`;
+    } else {
+    document.getElementById("even-or-odd").innerHTML = `${"Odd"}`;
+    }
+} 
 
 function sumTheNumbers() {
-    let sum = 0;
-    // Write the logic to sum the numbers 1 through 10
-    // using a for loop. Check the expected output
-    // on the assignment page
+    let sum = 1;
+    let total = 0; 
+    for (let sum = 1; sum < 11; sum++){ 
+    total += sum;}
+    // document.getElementById("sum-the-numbers").innerText = `${total}`;
+    sumTheNumbersElement.innerText = total;
+    
 
 }
 
 function createNumberArray() {
     const numberArray = [];
-
-    // Write the logic that loops 10 times and adds the value
-    // to numberArray each iteration. Check the expected output
-    // on the assignment page
-
+    for (let i = 1; i < 11; i++)
+    numberArray.push(i);
+    
+    // document.getElementById("create-number-array").innerHTML = numberArray;
+    createNumberArrayElement.innerText = numberArray
 }
 
 function render() {
-    // Call the created functions
-
+    evenOrOdd()
+    sumTheNumbers()
+    createNumberArray()
 }
 
 submissionBtn.addEventListener("click", function () {
